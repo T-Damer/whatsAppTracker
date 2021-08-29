3d4df7bc6a1cf8de4a6f185f2be2df4b53c78abd
T-Damer <dpancov64@gmail.com>
2021-08-29 18:58:59 +0300

    Added methods

--- macheronte/whatsapp/client.py
+++ macheronte/whatsapp/client.py
@@ -19,32 +19,80 @@ import ast
 
 class WhatsappClient:
 
-    def __init__(self):
-        pass
+    def __init__(self, browser: Browsers, profile_name: str):
+        options = Options()
+
+        if browser == Browsers.CHROME:
+            self.__webdriver = webdriver.Chrome(chrome_options=options)
+
+        self.__chat = None
+
+        self.__enter_action = ActionChains(self.__webdriver)
+        self.__enter_action.send_keys(Keys.ENTER)
+
+        self.__esc_action = ActionChains(self.__webdriver)
+        self.__esc_action.send_keys(Keys.ESCAPE)
+
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        self.__path = os.path.join(my_path, 'xpaths.cfg')
 
     def connect(self) -> bool:
-        pass
+        try:
+            self.__webdriver.get("https://web.whatsapp.com")
+            self.__wait = WebDriverWait(self.__webdriver, 5)
+        except:
+            return False
+        return True
 
     def close(self) -> bool:
-        pass
+        try:
+            self.__webdriver.quit()
+        except:
+            return False
+        return True
 
     def minimize_window(self):
-        pass
+        self.__webdriver.minimize_window()
 
     def maximize_window(self):
-        pass
+        self.__webdriver.maximize_window()
 
     def is_logged(self) -> bool:
-        pass
+        try:
+            self.__webdriver.find_element_by_xpath(self.__get_xpath("IS_LOGGED"))
+        except:
+            return False
+        return True
 
     def save_qrcode(self, filename: str) -> bool:
-        pass
+        im = self.__get_img_by_variables("QR_CODE")
+
+        if im != None:
+            im.save(filename)
+            return True
+        return False
 
     def save_header(self, filename: str) -> bool:
-        pass
+        im = self.__get_img_by_variables("HEADER")
+
+        if im != None:
+            im.save(filename)
+            return True
+        return False
 
     def get_header(self) -> bool:
-        pass
+        im = self.__get_img_by_variables("HEADER")
+
+        if im == None:
+            return None
+
+        width, height = im.size
+        im = im.crop((0, 0, width - width * 0.5, height))
+
+        b = BytesIO()
+        im.save(b, "PNG")
+        b.seek(0)
+        return b
 
     def open_chat(self, target: str) -> bool:
         pass
